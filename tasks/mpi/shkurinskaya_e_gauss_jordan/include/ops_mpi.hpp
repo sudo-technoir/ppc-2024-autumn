119af34b24e272dd65cbc3f950b7f2e40e670d7b
sudo-technoir <skurinskayae@gmail.com>
2024-11-30 19:59:40 +0300

    Add files via upload

---
+++ tasks/mpi/shkurinskaya_e_gauss_jordan/include/ops_mpi.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <gtest/gtest.h>
+
+#include <boost/mpi/collectives.hpp>
+#include <boost/mpi/communicator.hpp>
+#include <memory>
+#include <numeric>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "core/task/include/task.hpp"
+
+namespace shkurinskaya_e_gauss_jordan_mpi {
+
+class TestMPITaskSequential : public ppc::core::Task {
+ public:
+  explicit TestMPITaskSequential(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
+
+ private:
+  int n = 0;
+  std::vector<double> matrix;
+  std::vector<double> solution;
+};
+
+class TestMPITaskParallel : public ppc::core::Task {
+ public:
+  explicit TestMPITaskParallel(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
+
+ private:
+  struct diffPiece {
+    int index;
+    int rank;
+    double value;
+    template <class Archive>
+    void serialize(Archive& ar, unsigned int version) {
+      ar & index;
+      ar & value;
+    }
+  };
+
+  int n = 0;
+  std::vector<double> matrix;
+  std::vector<double> local_matrix;
+  std::vector<double> solution;
+  boost::mpi::communicator world;
+  std::vector<double> diag_elements;
+};
+
+}  // namespace shkurinskaya_e_gauss_jordan_mpi
