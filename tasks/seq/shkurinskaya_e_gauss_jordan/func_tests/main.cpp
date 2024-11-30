dbece761e9146c0081d89b55f5f4d3006e5eeac9
sudo-technoir <skurinskayae@gmail.com>
2024-11-30 21:55:53 +0300

    Update main.cpp

--- tasks/seq/shkurinskaya_e_gauss_jordan/func_tests/main.cpp
+++ tasks/seq/shkurinskaya_e_gauss_jordan/func_tests/main.cpp
@@ -58,7 +58,8 @@ TEST(shkurinskaya_e_gauss_jordan_seq, Test_Gauss_3x3) {
 TEST(shkurinskaya_e_gauss_jordan_seq, Test_Gauss_5x5) {
   int n = 5;
 
-  std::vector<double> in = {2, 3, -1, 5, 1, 8, 4, -2, 3, -1, 2, 10, -1, 5, 2, 3, -4, -3, 3, 2, 4, 1, -2, 6, 1, 1, 1, 1, 1, 4};
+  std::vector<double> in = {2, 3, -1, 5, 1, 8, 4, -2, 3, -1, 2, 10, -1, 5, 2, 
+                            3, -4, -3, 3, 2, 4, 1, -2, 6, 1, 1, 1, 1, 1, 4};
   std::vector<double> expected_output = {2.0, 0.142, 0.285, 0.571, 1.0};
   std::vector<double> out(n, 0.0);
 
@@ -82,7 +83,7 @@ TEST(shkurinskaya_e_gauss_jordan_seq, Test_Gauss_5x5) {
   }
 }
 
-  TEST(shkurinskaya_e_gauss_jordan_seq, Test_Gauss_Invalid_Data) {
+TEST(shkurinskaya_e_gauss_jordan_seq, Test_Gauss_Invalid_Data) {
   int n = 2;
 
   std::vector<double> in = {1, 2, 5};
@@ -101,39 +102,39 @@ TEST(shkurinskaya_e_gauss_jordan_seq, Test_Gauss_5x5) {
   ASSERT_EQ(gaussTaskSequential.validation(), false);
 }
 
-  TEST(shkurinskaya_e_gauss_jordan_seq, Test_Gauss_Zero_Diag) {
-    int n = 3;
-    std::vector<double> in = {0, 1, 1, 1, 2, 1, 2, 2, 2, 2, 4, 3};
-    std::vector<double> out(n, 0.0);
+TEST(shkurinskaya_e_gauss_jordan_seq, Test_Gauss_Zero_Diag) {
+  int n = 3;
+  std::vector<double> in = {0, 1, 1, 1, 2, 1, 2, 2, 2, 2, 4, 3};
+  std::vector<double> out(n, 0.0);
 
-    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
-    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(&n));
-    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
-    taskDataSeq->inputs_count.emplace_back(in.size() / (n + 1));
-    taskDataSeq->inputs_count.emplace_back(in.size());
-    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
-    taskDataSeq->outputs_count.emplace_back(out.size());
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(&n));
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+  taskDataSeq->inputs_count.emplace_back(in.size() / (n + 1));
+  taskDataSeq->inputs_count.emplace_back(in.size());
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  taskDataSeq->outputs_count.emplace_back(out.size());
 
-    shkurinskaya_e_gauss_jordan_seq::TestTaskSequential gaussTaskSequential(taskDataSeq);
+  shkurinskaya_e_gauss_jordan_seq::TestTaskSequential gaussTaskSequential(taskDataSeq);
 
-    ASSERT_FALSE(gaussTaskSequential.validation());
-  }
+  ASSERT_FALSE(gaussTaskSequential.validation());
+}
 
-  TEST(shkurinskaya_e_gauss_jordan_seq, Test_Gauss_Overdetermined_System) {
-    int n = 3;
+TEST(shkurinskaya_e_gauss_jordan_seq, Test_Gauss_Overdetermined_System) {
+  int n = 3;
 
-    std::vector<double> in = {1, 1, 1, 6, 0, 2, 5, -4, 2, 5, -1, 27, 1, 1, 1, 6};
-    std::vector<double> out(n, 0.0);
+  std::vector<double> in = {1, 1, 1, 6, 0, 2, 5, -4, 2, 5, -1, 27, 1, 1, 1, 6};
+  std::vector<double> out(n, 0.0);
 
-    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
-    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(&n));
-    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
-    taskDataSeq->inputs_count.emplace_back(in.size() / (n + 1));
-    taskDataSeq->inputs_count.emplace_back(in.size());
-    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
-    taskDataSeq->outputs_count.emplace_back(out.size());
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(&n));
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+  taskDataSeq->inputs_count.emplace_back(in.size() / (n + 1));
+  taskDataSeq->inputs_count.emplace_back(in.size());
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  taskDataSeq->outputs_count.emplace_back(out.size());
 
-    shkurinskaya_e_gauss_jordan_seq::TestTaskSequential gaussTaskSequential(taskDataSeq);
+  shkurinskaya_e_gauss_jordan_seq::TestTaskSequential gaussTaskSequential(taskDataSeq);
 
-    ASSERT_FALSE(gaussTaskSequential.validation());
-  }
+  ASSERT_FALSE(gaussTaskSequential.validation());
+}
