c85e1cc605f831f3d447458d69aa55cabfbab3be
sudo-technoir <skurinskayae@gmail.com>
2024-12-01 02:01:57 +0300

    Update ops_mpi.cpp

--- tasks/mpi/shkurinskaya_e_gauss_jordan/src/ops_mpi.cpp
+++ tasks/mpi/shkurinskaya_e_gauss_jordan/src/ops_mpi.cpp
@@ -31,7 +31,7 @@ bool shkurinskaya_e_gauss_jordan_mpi::TestMPITaskSequential::validation() {
     std::cout << "Validation failed: invalid dimensions (rows or columns cannot be zero or negative)!" << std::endl;
     return false;
   }
-  size_t expectedSize = static_cast<size_t>(numRows * numCols);
+  auto expectedSize = static_cast<size_t>(numRows * numCols);
   if (taskData->inputs_count[1] != expectedSize) {
     std::cout << "Validation failed: matrix size mismatch! Expected " << expectedSize << " elements, but found "
               << taskData->inputs_count[1] << " elements." << std::endl;
@@ -122,7 +122,7 @@ bool shkurinskaya_e_gauss_jordan_mpi::TestMPITaskParallel::validation() {
       std::cout << "Validation failed: invalid dimensions (rows or columns cannot be zero or negative)!" << std::endl;
       return false;
     }
-    size_t expectedSize = static_cast<size_t>(numRows * numCols);
+    auto expectedSize = static_cast<size_t>(numRows * numCols);
     if (taskData->inputs_count[1] != expectedSize) {
       std::cout << "Validation failed: matrix size mismatch! Expected " << expectedSize << " elements, but found "
                 << taskData->inputs_count[1] << " elements." << std::endl;
